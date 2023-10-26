48cbca5a6f87aea1d4a986e1ddd3458b796da951
Alejo Apsega <alex@iMac.local>
2023-10-26 19:18:43 -0300

    Carga de archivos para 2da PreEntrega de JavaScript

--- script.js
+++ script.js
@@ -8,29 +8,28 @@ while(nombre === ""){
 } 
 //Saludo de bienvenida
 alert(`Bienvenido ${nombre} a la tienda`)
-//Preguntar filtrado de 2 productos
-const producto1 = ["Nombre", "Direccion", 4000]
-const producto2 = ["Apellido", "Apellido", 5000]
-console.log(producto1)
-console.log(producto2)
+//Filtrado de 2 productos
+const producto1 = products.find(product => product.category === "jewelery")
+const producto2 = products.find(product => product.category === "men's clothing")
 //Se ofrecen las categorías disponibles
-alert(`Estas son las categorias de productos disponibles: 1.- "Producto 1" 2.- "Producto 2"`)
+alert(`Estas son las categorias de productos disponibles: 1.- "${producto1.title}" 2.- "${producto2.title}"`)
 //Se solicita elegir una opción
 let categoria = 0
 while(categoria !==1 && categoria !==2){
-    categoria = parseInt(prompt(`Estas son las categorías de productos disponibles: 1.- "Producto 1" 2.- "Producto 2". Seleccione el número de opción elegida:`))
+    categoria = parseInt(prompt(`Estas son las categorías de productos disponibles: 1.- "${producto1.title}" 2.- "${producto2.title}". Seleccione el número de opción elegida:`))
     if(categoria !== 1 && categoria !== 2){
         alert(`Por favor seleccione sólo una de las opciones disponibles.`)
     }
 } 
 //Selección de producto - creación de carrito de compras
 let productoElegido = []
-productoElegido.push(producto1[categoria])
-console.log(productoElegido)
-//Si no se encuentra el producto, hay que preguntar nuevamente
-
+if(categoria === 1){
+    productoElegido = producto1
+} else{
+    productoElegido = producto2
+}
 //Se solicita confirme o rechace la elección
-const respuesta = parseInt(confirm(`Eligió ${NOMBREPRODUCTO}, ${DESCRIPCION} y cuyo precio es ${PRECIO}. ¿Desea realizar la compra?`))
+const respuesta = confirm(`Eligió ${productoElegido.title}, ${productoElegido.description} y cuyo precio es $${productoElegido.price}. ¿Desea realizar la compra?`)
 //Respuesta final
 if(respuesta === true){
     //El plazo de envío estàndar es de 5 días
@@ -40,5 +39,6 @@ if(respuesta === true){
     let mes = meses[(parseInt(fechaDeEnvio.getMonth())-1)]
     alert(`Gracias por su compra. Tiene un envío de 5 (cinco) días a partir de hoy, así que llegará el día ${fechaDeEnvio.getDate()} de ${mes}. Hasta la próxima.`)
 
-
+}else{
+    alert(`Gracias por su visita. Hasta la próxima.`)
 }
